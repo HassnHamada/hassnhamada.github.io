fbc0d967969721e9e08f441530a144487d6c7bd3
Hassn Hamada <hassneltokhy4@gmail.com>
2025-04-08 21:41:40 +0200

    Add header section

---
+++ apps/portfolio/src/app/Sections/Header.tsx
@@ -0,0 +1,73 @@
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import { Box, Button, Card, CardActions, Grid, Typography } from '@mui/material'
+import { useMemo } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  const links = useMemo(() => [
+    {
+      icon: <LinkedInIcon />,
+      link: 'https://www.linkedin.com/in/hassn-hamada-659025188',
+      text: 'LinkedIn',
+      color: 'rgb(29, 122, 214)'
+    },
+    {
+      icon: <GitHubIcon />,
+      link: "https://github.com/hassnhamada",
+      text: 'GitHub',
+      color: 'rgb(218, 218, 218)'
+    }
+  ], [])
+  return (
+    <Box>
+      <Grid container>
+        <Grid size={{ xs: 12 }} pb={2}>
+          <Typography variant="h4" component="h1">
+            Hello
+            <span role="img" aria-label="waving hand">
+              👋
+            </span>
+            , I'm <strong>Hassn Hamada</strong>
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 12 }} pb={2}>
+          <Typography variant="body1" fontSize={20} component="p">
+            Software Engineer
+            <span role="img" aria-label="man coding">
+              👨‍💻
+            </span>
+            , Problem Solver
+            <span role="img" aria-label="puzzle piece">
+              🧩
+            </span>
+            , and an AI Enthusiast
+            <span role="img" aria-label="robot">
+              🤖
+            </span>
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          <Card>
+            <CardActions>
+              {links.map((link, index) =>
+                <Button
+                  key={index}
+                  component={Link}
+                  to={link.link}
+                  startIcon={link.icon}
+                  variant="text"
+                  sx={{ color: link.color, width: '100%', height: '100%' }}
+                >
+                  <Typography variant="body1" component="span">
+                    {link.text}
+                  </Typography>
+                </Button>
+              )}
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
--- apps/portfolio/src/app/app.tsx
+++ apps/portfolio/src/app/app.tsx
@@ -1,8 +1,21 @@
-// Uncomment this line to use CSS modules
-// import styles from './app.module.css';
+import { Box, Grid } from '@mui/material'
+import Header from './Sections/Header'
 
 export function App() {
-  return <div>HI</div>
+  return (
+    <Box p={2}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        columns={{ xs: 6, md: 8, lg: 12 }}
+      >
+        <Grid size={{ xs: 6 }}>
+          <Header />
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default App
